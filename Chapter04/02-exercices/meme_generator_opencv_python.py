3e6df5b085cfe210d70891bca72e56f75ac70e68
Alberto <fernandezvillan.alberto@gmail.com>
2019-02-25 13:36:20 +0100

    formatting

--- Chapter04/02-exercices/meme_generator_opencv_python.py
+++ Chapter04/02-exercices/meme_generator_opencv_python.py
@@ -9,10 +9,9 @@ import matplotlib.pyplot as plt
 
 
 def show_with_matplotlib(img, title):
-    """Shows an image using matplotlib capabilities
+    """Shows an image using matplotlib capabilities"""
 
-    """
-    # Convert BGR image to RGB
+    # Convert BGR image to RGB:
     img_RGB = img[:, :, ::-1]
 
     # Show the image using matplotlib:
@@ -21,7 +20,7 @@ def show_with_matplotlib(img, title):
     plt.show()
 
 
-# Dictionary containing some colors
+# Dictionary containing some colors:
 colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 'red': (0, 0, 255), 'yellow': (0, 255, 255),
           'magenta': (255, 0, 255), 'cyan': (255, 255, 0), 'white': (255, 255, 255), 'black': (0, 0, 0),
           'gray': (125, 125, 125), 'rand': np.random.randint(0, high=256, size=(3,)).tolist(),
@@ -30,10 +29,10 @@ colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 'red': (0, 0, 255), 'yellow
 # We load the image 'lenna.png':
 image = cv2.imread("lenna.png")
 
-# Write some text (up)
+# Write some text (up):
 cv2.putText(image, 'Hello World', (10, 30), cv2.FONT_HERSHEY_TRIPLEX, 0.8, colors['green'], 1, cv2.LINE_AA)
 
-# Write some text (down)
+# Write some text (down):
 cv2.putText(image, 'Goodbye World', (10, 200), cv2.FONT_HERSHEY_TRIPLEX, 0.8, colors['red'], 1, cv2.LINE_AA)
 
 # Show image:
