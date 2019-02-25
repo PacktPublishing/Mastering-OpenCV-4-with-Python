d2707c9905e8385a9096e10081759b9bc8408842
Alberto <fernandezvillan.alberto@gmail.com>
2019-02-25 13:26:52 +0100

    formatting

--- Chapter04/01-chapter-content/text_drawing.py
+++ Chapter04/01-chapter-content/text_drawing.py
@@ -9,10 +9,9 @@ import matplotlib.pyplot as plt
 
 
 def show_with_matplotlib(img, title):
-    """Shows an image using matplotlib capabilities
+    """Shows an image using matplotlib capabilities"""
 
-    """
-    # Convert BGR image to RGB
+    # Convert BGR image to RGB:
     img_RGB = img[:, :, ::-1]
 
     # Show the image using matplotlib:
@@ -21,14 +20,14 @@ def show_with_matplotlib(img, title):
     plt.show()
 
 
-# Dictionary containing some colors
+# Dictionary containing some colors:
 colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 'red': (0, 0, 255), 'yellow': (0, 255, 255),
           'magenta': (255, 0, 255), 'cyan': (255, 255, 0), 'white': (255, 255, 255), 'black': (0, 0, 0),
           'gray': (125, 125, 125), 'rand': np.random.randint(0, high=256, size=(3,)).tolist(),
           'dark_gray': (50, 50, 50), 'light_gray': (220, 220, 220)}
 
 # We create the canvas to draw: 120 x 512 pixels, 3 channels, uint8 (8-bit unsigned integers)
-# We set background to black using np.zeros()
+# We set background to black using np.zeros():
 image = np.zeros((120, 512, 3), dtype="uint8")
 
 # If you want another background color you can do the following:
