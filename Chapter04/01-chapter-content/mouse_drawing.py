9ee1026bf7460d6101569fbe6a87e6d812a8d23a
Alberto <fernandezvillan.alberto@gmail.com>
2019-02-25 13:18:23 +0100

    formatting

--- Chapter04/01-chapter-content/mouse_drawing.py
+++ Chapter04/01-chapter-content/mouse_drawing.py
@@ -5,9 +5,8 @@ Example to show how to draw circles using the mouse events in OpenCV
 # Import required packages:
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Dictionary containing some colors
+# Dictionary containing some colors:
 colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 'red': (0, 0, 255), 'yellow': (0, 255, 255),
           'magenta': (255, 0, 255), 'cyan': (255, 255, 0), 'white': (255, 255, 255), 'black': (0, 0, 0),
           'gray': (125, 125, 125), 'rand': np.random.randint(0, high=256, size=(3,)).tolist(),
@@ -31,20 +30,20 @@ def draw_circle(event, x, y, flags, param):
 
 
 # We create the canvas to draw: 600 x 600 pixels, 3 channels, uint8 (8-bit unsigned integers)
-# We set the background to black using np.zeros()
+# We set the background to black using np.zeros():
 image = np.zeros((600, 600, 3), dtype="uint8")
 
-# We create a named window where the mouse callback will be established
+# We create a named window where the mouse callback will be established:
 cv2.namedWindow('Image mouse')
 
-# We set the mouse callback function to 'draw_circle'
+# We set the mouse callback function to 'draw_circle':
 cv2.setMouseCallback('Image mouse', draw_circle)
 
 while True:
     # Show image 'Image mouse':
     cv2.imshow('Image mouse', image)
 
-    # Continue until 'q' is pressed
+    # Continue until 'q' is pressed:
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
