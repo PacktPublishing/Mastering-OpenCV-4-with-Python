2d7d6732893dcc01ba89a71c335e312456d098af
Alberto <fernandezvillan.alberto@gmail.com>
2019-02-25 13:22:27 +0100

    formatting

--- Chapter04/01-chapter-content/shift_parameter.py
+++ Chapter04/01-chapter-content/shift_parameter.py
@@ -9,10 +9,9 @@ import matplotlib.pyplot as plt
 
 
 def show_with_matplotlib(img, title):
-    """Shows an image using matplotlib capabilities
+    """Shows an image using matplotlib capabilities"""
 
-    """
-    # Convert BGR image to RGB
+    # Convert BGR image to RGB:
     img_RGB = img[:, :, ::-1]
 
     # Show the image using matplotlib:
@@ -22,28 +21,28 @@ def show_with_matplotlib(img, title):
 
 
 def draw_float_circle(img, center, radius, color, thickness=1, lineType=8, shift=4):
-    """Wrapper function to draw float-coordinate circles
+    """Wrapper function to draw float-coordinate circles"""
 
-    """
     factor = 2 ** shift
     center = (int(round(center[0] * factor)), int(round(center[1] * factor)))
     radius = int(round(radius * factor))
     cv2.circle(img, center, radius, color, thickness, lineType, shift)
 
 
-# Dictionary containing some colors
+# Dictionary containing some colors:
 colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 'red': (0, 0, 255), 'yellow': (0, 255, 255),
           'magenta': (255, 0, 255), 'cyan': (255, 255, 0), 'white': (255, 255, 255), 'black': (0, 0, 0),
           'gray': (125, 125, 125), 'rand': np.random.randint(0, high=256, size=(3,)).tolist(),
           'dark_gray': (50, 50, 50), 'light_gray': (220, 220, 220)}
 
 # We create the canvas to draw: 800 x 800 pixels, 3 channels, uint8 (8-bit unsigned integers)
-# We set the background to black using np.zeros()
+# We set the background to black using np.zeros():
 image = np.zeros((800, 800, 3), dtype="uint8")
 
 # If you want another background color you can do the following:
 image[:] = colors['light_gray']
 
+# Draw the circles:
 draw_float_circle(image, (299, 299), 300, colors['red'], 1, 8, 0)
 draw_float_circle(image, (299.9, 299.9), 300, colors['green'], 1, 8, 1)
 draw_float_circle(image, (299.99, 299.99), 300, colors['blue'], 1, 8, 2)
