e2c5c88348213205866eeb212aa5134a4ccec6c7
Alberto <fernandezvillan.alberto@gmail.com>
2019-02-25 13:32:12 +0100

    formatting

--- Chapter04/02-exercices/circle_polygon.py
+++ Chapter04/02-exercices/circle_polygon.py
@@ -9,10 +9,9 @@ import matplotlib.pyplot as plt
 
 
 def show_with_matplotlib(img, title):
-    """Shows an image using matplotlib capabilities
+    """Shows an image using matplotlib capabilities"""
 
-    """
-    # Convert BGR image to RGB
+    # Convert BGR image to RGB:
     img_RGB = img[:, :, ::-1]
 
     # Show the image using matplotlib:
@@ -21,25 +20,26 @@ def show_with_matplotlib(img, title):
     plt.show()
 
 
-# Dictionary containing some colors
+# Dictionary containing some colors:
 colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 'red': (0, 0, 255), 'yellow': (0, 255, 255),
           'magenta': (255, 0, 255), 'cyan': (255, 255, 0), 'white': (255, 255, 255), 'black': (0, 0, 0),
           'gray': (125, 125, 125), 'rand': np.random.randint(0, high=256, size=(3,)).tolist(),
           'dark_gray': (50, 50, 50), 'light_gray': (220, 220, 220)}
 
 # We create the canvas to draw: 640 x 640 pixels, 3 channels, uint8 (8-bit unsigned integers)
-# We set background to black using np.zeros()
+# We set background to black using np.zeros():
 image = np.zeros((640, 640, 3), dtype="uint8")
 
 # If you want another background color you can do the following:
 # image[:] = colors['light_gray']
 image.fill(255)
 
+# Create the necessary (12 in this case) points to build the circle polygon:
 pts = np.array(
     [(600, 320), (563, 460), (460, 562), (320, 600), (180, 563), (78, 460), (40, 320), (77, 180), (179, 78), (319, 40),
      (459, 77), (562, 179)])
 
-# Reshape to shape (number_vertex, 1, 2)
+# Reshape to shape (number_vertex, 1, 2):
 pts = pts.reshape((-1, 1, 2))
 
 # Call cv2.polylines() to build the polygon:
