3393fecd3a1024beb0cf57efe9723c4f36330007
Alberto <fernandezvillan.alberto@gmail.com>
2019-02-25 13:29:50 +0100

    formatting

--- Chapter04/01-chapter-content/text_drawing_fonts.py
+++ Chapter04/01-chapter-content/text_drawing_fonts.py
@@ -9,10 +9,9 @@ import matplotlib.pyplot as plt
 
 
 def show_with_matplotlib(img, title):
-    """Shows an image using matplotlib capabilities
+    """Shows an image using matplotlib capabilities"""
 
-    """
-    # Convert BGR image to RGB
+    # Convert BGR image to RGB:
     img_RGB = img[:, :, ::-1]
 
     # Show the image using matplotlib:
@@ -21,27 +20,28 @@ def show_with_matplotlib(img, title):
     plt.show()
 
 
-# Dictionary containing some colors
+# Dictionary containing some colors:
 colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 'red': (0, 0, 255), 'yellow': (0, 255, 255),
           'magenta': (255, 0, 255), 'cyan': (255, 255, 0), 'white': (255, 255, 255), 'black': (0, 0, 0),
           'gray': (125, 125, 125), 'rand': np.random.randint(0, high=256, size=(3,)).tolist(),
           'dark_gray': (50, 50, 50), 'light_gray': (220, 220, 220)}
 
-# Dictionary containing some strings to output
+# Dictionary containing some strings to output:
 fonts = {0: "FONT HERSHEY SIMPLEX", 1: "FONT HERSHEY PLAIN", 2: "FONT HERSHEY DUPLEX", 3: "FONT HERSHEY COMPLEX",
          4: "FONT HERSHEY TRIPLEX", 5: "FONT HERSHEY COMPLEX SMALL ", 6: "FONT HERSHEY SCRIPT SIMPLEX",
          7: "FONT HERSHEY SCRIPT COMPLEX"}
 
-# Dictionary containing the index for each color
+# Dictionary containing the index for each color:
 index_colors = {0: 'blue', 1: 'green', 2: 'red', 3: 'yellow', 4: 'magenta', 5: 'cyan', 6: 'black', 7: 'dark_gray'}
 
 # We create the canvas to draw: 650 x 650 pixels, 3 channels, uint8 (8-bit unsigned integers)
-# We set background to black using np.zeros()
+# We set background to black using np.zeros():
 image = np.zeros((650, 650, 3), dtype="uint8")
 
 # If you want another background color you can do the following:
 image[:] = colors['white']
 
+# Write the text using all the OpenCV fonts:
 position = (10, 30)
 for i in range(0, 8):
     print("i index value: '{}' text: '{}' + color: '{}' = '{}'".format(i, fonts[i].lower(), index_colors[i],
