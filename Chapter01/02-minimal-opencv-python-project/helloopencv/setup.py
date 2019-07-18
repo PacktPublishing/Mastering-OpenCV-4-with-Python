e56da9f069225e629dd26c9f174b2bc317afe8ca
Alberto Fernández Villán <alberto.fernandez@fundacionctic.org>
2019-07-18 20:16:43 +0200

    Update setup.py

--- Chapter01/02-minimal-opencv-python-project/helloopencv/setup.py
+++ Chapter01/02-minimal-opencv-python-project/helloopencv/setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     description='An example python opencv project',
     long_description=open('README.rst').read(),
-    install_requires=['numpy','opencv-python'],
+    install_requires=['numpy','opencv-contrib-python'],
     url='https://github.com/albertofernandez',
     author='Alberto Fernandez',
     author_email='fernandezvillan.alberto@gmail.com'
